820e66d17ce86c5ff84fa961134f586d35b272a6
fernando <fsosat1400@alumno.ipn.mx>
2023-02-12 20:43:57 -0600

    Add the start in background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import backgroud from './sass/themes/background.module.scss';
 
 function App() {
   return (
-    <section className={backgroud.background} >
+    <section className={`${backgroud.background} ${backgroud.start}`} >
       <h1 className={text.title}>we're launching soon</h1>
     </section>
   );
