9299395c354d3d535663c22501c7c82ccecb661f
fernando <fsosat1400@alumno.ipn.mx>
2023-02-12 22:54:47 -0600

    Add the style of the time and the text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Timer } from "./components/Timer";
 import text from "./sass/base/text.module.scss";
-import backgroud from './sass/themes/background.module.scss';
+import background from './sass/themes/background.module.scss';
 
 function App() {
   return (
-    <section className={`${backgroud.background} ${backgroud.start}`} >
+    <>
+    <section className={`${background.background} ${background.start}`} >
       <h1 className={text.title}>we're launching soon</h1>
+
+      <Timer/>
+      {/* <footer className={`${background.mountains}`}></footer> */}
     </section>
+    </>
   );
 }
 
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import text from '../sass/layout/text.module.scss';
-
+import React from "react";
+import time from "../sass/layout/timer.module.scss";
+import text from "../sass/base/text.module.scss";
 export const Timer = () => {
   return (
+    <section className={`${time.timer}`}>
+      <article>
+        <div className={`${time.time}`}>
+          <span className={`${time.line}`}></span>
+          08
+        </div>
+        <div className={`${text.detail}`}>days</div>
+      </article>
 
-    <section>
-        
+      <article>
+        <div className={`${time.time}`}>23</div>
+        <div className={`${text.detail}`}>hours</div>
+      </article>
+
+      <article>
+        <div className={`${time.time}`}>55</div>
+        <div className={`${text.detail}`}>minutes</div>
+      </article>
+
+      <article>
+        <div className={`${time.time}`}>41</div>
+        <div className={`${text.detail}`}>seconds</div>
+      </article>
     </section>
-  )
-}
+  );
+};
