6a4270c4d8a18a8a2868d84ca5da3d2606384274
fernando <fsosat1400@alumno.ipn.mx>
2023-02-12 20:40:07 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import text from "./sass/base/text.module.scss";
+import backgroud from './sass/themes/background.module.scss';
+
+function App() {
+  return (
+    <section className={backgroud.background} >
+      <h1 className={text.title}>we're launching soon</h1>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Timer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import text from '../sass/layout/text.module.scss';
+
+export const Timer = () => {
+  return (
+
+    <section>
+        
+    </section>
+  )
+}
