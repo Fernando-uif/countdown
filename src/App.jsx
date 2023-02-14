3a169fe4dec9ac08a9eb4f0f41e21456ad6fbca3
fernando <fsosat1400@alumno.ipn.mx>
2023-02-13 20:28:47 -0600

    Add the footer social media

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Timer } from "./components/Timer";
 import text from "./sass/base/text.module.scss";
-import background from './sass/themes/background.module.scss';
+import background from "./sass/themes/background.module.scss";
+import instagram from "./assets/images/icon-instagram.svg";
+import facebook from "./assets/images/icon-facebook.svg";
+import pinterest from "./assets/images/icon-pinterest.svg";
 
 function App() {
   return (
     <>
-    <section className={`${background.background} ${background.start}`} >
-      <h1 className={text.title}>we're launching soon</h1>
+      <section className={`${background.background} ${background.start}`}>
+        <h1 className={text.title}>we're launching soon</h1>
 
-      <Timer/>
-      {/* <footer className={`${background.mountains}`}></footer> */}
-    </section>
+        <Timer />
+      </section>
+      <footer className={`${background.mountains}`}>
+        <section className={`${background.containSocial}`}>
+          <img src={facebook} className={background.icon} alt="" />
+          <img src={pinterest} className={background.icon} alt="" />
+          <img src={instagram} className={background.icon} alt="" />
+        </section>
+      </footer>
     </>
   );
 }
